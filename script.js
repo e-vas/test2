6dae111dc2737f01523322000e145e8dfdf79e52
e-vas <96337026+e-vas@users.noreply.github.com>
2023-07-08 12:35:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,28 +9,37 @@ let todos = [
 
 ];
 
+// Найти кнопку ADD и добавить к ней событие
 const addEvent = document.querySelector('button'); 
 addEvent.addEventListener('click', addTask);
 
+// Действия по кнопке ADD
 function addTask(event) {
     event.preventDefault();
-    let taskName = document.querySelector('input').value;
-    let lastId = todos.at(-1).id + 1;
-    todos.push({id: lastId, title: taskName});
-    updateTodos ({id: lastId, title: taskName});    
-    //console.log(todos);
+    let taskName = document.querySelector('input').value; 
+    let lastId;
+    if (todos.length === 0) {
+        lastId = 0;
+    } else {
+        lastId = todos.at(-1).id + 1;
+    }
+    
+    todos.push({id: lastId, title: taskName}); //Добавление нового эл-та в массив
+    updateTodos ({id: lastId, title: taskName}); //Функция добавления на стр    
+    document.querySelector('input').value = ""; //Сброс значения в строке
+    findDellEvent (); //Перезапуск поиска всех кнопок удаления включая новую
 };
 
-
+// Отображение всех элементов массива todos
 for (i in todos) {
         updateTodos (todos[i]);
-
 };
 
-
+//  Добавление элементов из массива на страницу
 function updateTodos ({ id, title }){
         let li = document.createElement('li');
-        li.innerHTML = " ";
+        li.innerHTML = "";
+        li.id = id;
                 let inputForm = document.createElement('input');
                 inputForm.type = "checkbox";
                 li.append(inputForm);
@@ -48,8 +57,35 @@ function updateTodos ({ id, title }){
                         imgD.height="17";
                         buttonD.append(imgD);
 
-                li.append(buttonD);
+        li.append(buttonD);
 
         toDoList.append(li);        
 }
 
+// Поиск всех кнопок удаления + добавление события для каждой li 
+let dellEvent = [];
+function findDellEvent () {  
+        dellEvent = document.querySelectorAll('li > button'); 
+        dellEvent.forEach((item) => item.addEventListener('click', dellTask));
+        
+        
+}
+findDellEvent ();
+
+// удаление элемента со стр и массива
+function dellTask (liID){
+        //удаление эл-та со стр
+        liID = this.parentNode.id;
+        const dellItem = document.getElementById(liID);
+        dellItem.remove();
+
+        //удаление из массива
+        liIndex = todos.findIndex(i => i.id === liID);
+        todos.splice(liIndex, 1);
+
+        //тест
+        console.log("Deleted li ID: " + liID + ", li INDEX: " + liIndex);
+        for (i in todos){
+                console.log(todos[i]);
+        }
+};
